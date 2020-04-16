3e6def3aefc4d1ec7afbde3ef13a701f43af3b2d
liwei.ong <liwei.ong@go-jek.com>
2020-04-16 21:31:51 +0800

    feat: Use newman's cli output as server response

--- bin/server.js
+++ bin/server.js
@@ -1,5 +1,18 @@
 const http = require('http');
 const newman = require('newman');
+const PostmanCLIReporter = require('newman/lib/reporters/cli');
+const util = require('util');
+
+const listeners = {};
+
+function interceptStdout() {
+  process.stdout.write = (function (write) {
+    return (...args) => {
+      write.apply(process.stdout, args);
+      Object.values(listeners).forEach((l) => l(...args));
+    };
+  })(process.stdout.write);
+}
 
 const port = process.env.NEWMAN_PORT || 8080;
 
@@ -27,18 +40,24 @@ function runCollection(collection) {
   });
 }
 
+interceptStdout();
 http
   .createServer(async function (req, res) {
+    const listenerId = Date.now();
+    const resultArr = [];
+    listeners[listenerId] = (string) => {
+      resultArr.push(string);
+    };
     const collection = await extractData(req);
     const [err, summary] = await runCollection(collection);
     const success = !err && summary.error && result.run.failures.length === 0;
+    delete listeners[listenerId];
+    const result = util.format('\n' + resultArr.join('') + '\n');
 
     if (success) {
-      res.end();
+      res.end(result);
     } else {
-      res.writeHead(400).end();
+      res.writeHead(400).end(result);
     }
   })
   .listen(port);
-
-console.log(`[newman-server] Listening on port ${port}`);
